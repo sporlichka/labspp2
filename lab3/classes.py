64c6a31e5cd67dd97e24584b50520e26c75a4866
sporlichka <tvooybatya@gmail.com>
2024-02-01 16:32:19 +0600

    classes

---
+++ lab3/classes.py
@@ -0,0 +1,15 @@
+#1
+class Myclass:
+    x = 5
+#class created
+#2
+class Myclass:
+    x = 5
+p1 = Myclass()
+#object of myclass called p1
+#3
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
