485edca219398dd6d6815ab860de455c211ad3e1
sporlichka <tvooybatya@gmail.com>
2024-02-03 04:39:12 +0600

    another change

--- lab3/functions1.py
+++ lab3/functions1.py
@@ -84,6 +84,8 @@ def spy_game(nums):
       count += 1
     if count == 2 and nums[i] == 7:
       return True
+    elif count != 2 and nums[i] == 7:
+      return False
   return False
 
 print(spy_game([1,2,4,0,0,7,5])) #--> True
