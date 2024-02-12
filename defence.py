e113bdfc0a7c8703c1fd7d3dee472c7b554a82d5
sporlichka <tvooybatya@gmail.com>
2024-02-12 22:28:30 +0600

    defence changed

--- defence.py
+++ defence.py
@@ -7,18 +7,17 @@ def cross(n):
                 for_pr += '*'
             else: for_pr += ' '
         print(for_pr)
-cross(9)
+cross(10)
 #2
 def square(n):
     str1 = "*"*n
     print(str1)
     for i in range(n-2):
-        str2 = ''
         for j in range(n):
             if j == 0 or j == n - 1:
-                str2 += '*'
-            else: str2 += ' '
-        print(str2)
+                print('*', end = '')
+            else: print(' ', end = '')
+        print('')
     print(str1)
 square(5)
 #3
@@ -27,10 +26,13 @@ class Elephant:
         self.weight = weight
         self.height = height
         self.age = age
-    def set_name(self, name):
+    def set_name(self):
+        name = input('write a name \n')
         self.name = name
     def get_name(self):
         print(self.name)
+        print(self.height)
+        print(self.weight)
 class Baby_El(Elephant):
     def __init__(self, weight, height, age, toys):
         super().__init__(weight, height, age)
@@ -40,7 +42,13 @@ class Baby_El(Elephant):
     def get_friend(self):
         print(self.friend)
 eve = Baby_El(25, 34, 2, ['Giraffe', 'bob'])
-eve.set_name('wall-e')
+eve.set_name()
 eve.get_name()
 eve.set_friend('zebra')
 eve.get_friend()
+#4
+arr = [1, 2, 3, 4, 5, 6, 7]
+arr.insert(3, 3.5)
+arr.append(9)
+arr = arr[::-1]
+print(*arr)
