07f13418029098b9f78084af02e81668b547b257
sporlichka <tvooybatya@gmail.com>
2024-03-03 22:53:49 +0500

    lab6

--- lab6/fil.py
+++ lab6/fil.py
@@ -1,7 +1,17 @@
+'''
+Write a Python program to list only directories, files and all directories, files in a specified path.
+Write a Python program to check for access to a specified path. Test the existence, readability, writability and executability of the specified path
+Write a Python program to test whether a given path exists or not. If the path exist find the filename and directory portion of the given path.
+Write a Python program to count the number of lines in a text file.
+Write a Python program to write a list to a file.
+Write a Python program to generate 26 text files named A.txt, B.txt, and so on up to Z.txt
+Write a Python program to copy the contents of a file to another file
+Write a Python program to delete file by specified path. Before deleting check for access and whether a given path exists or not.
+'''
 import os
 import string
 #1
-path = r'C:\Users\User\Desktop\PP2'
+path = r'C:\Users\Данис\Desktop\another pp2'
 print("Only directories:")
 print([name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))])
 print("\nOnly files:")
@@ -41,7 +51,7 @@ print("Number of lines in file: ", count_lines(path))
 string = str(input())
 s = string.split()
 
-path = r'C:\Users\User\Desktop\PP2\lab 6\test.txt'
+path = r'C:\Users\Данис\Desktop\another pp2\lab6\test.txt'
 
 with open('test.txt', 'w+') as f:
     for items in s:
